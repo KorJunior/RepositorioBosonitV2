b443f9036e7570bbfc0400466e7c8d54a8c1a4d3
Korodjouma Junior Fofana Coulibaly <kjfofana01@larioja.edu.es>
2024-03-22 08:09:50 +0100

    cambio de RamdomAcess por BuffedWrite

--- LecturaDeArchivosFormatoCobol/src/main/java/org/example/GeneradorDeFichero.java
+++ LecturaDeArchivosFormatoCobol/src/main/java/org/example/GeneradorDeFichero.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import com.github.javafaker.Faker;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.Locale;
 
 public class GeneradorDeFichero {
@@ -11,7 +13,8 @@ public class GeneradorDeFichero {
         Faker faker = new Faker(new Locale("es", "MX"));
 
         try {
-            RandomAccessFile file = new RandomAccessFile("vehiculos.txt", "rw");
+            BufferedWriter bw = new BufferedWriter(new FileWriter("vehiculos.txt"));
+
 
             for (int i = 0; i < 50; i++) {
                 long id = faker.number().numberBetween(1L, 999999999L);
@@ -21,17 +24,17 @@ public class GeneradorDeFichero {
                 float importe = (float) faker.number().randomDouble(2, 10000, 50000);
                 int ano = faker.number().numberBetween(1990, 2022);
 
-                file.writeBytes(padRight(String.valueOf(id), 10));
-                file.writeBytes(padRight(marca, 20));
-                file.writeBytes(padRight(modelo, 20));
-                file.writeBytes(padRight(color, 10));
-                file.writeBytes(padRight(String.format("%.2f", importe), 9));
-                file.writeBytes(padRight(String.valueOf(ano), 5));
+                bw.write(padRight(String.valueOf(id), 10));
+                bw.write(padRight(marca, 20));
+                bw.write(padRight(modelo, 20));
+                bw.write(padRight(color, 10));
+                bw.write(padRight(String.format("%.2f", importe), 9));
+                bw.write(padRight(String.valueOf(ano), 5));
 
-                file.writeBytes(System.lineSeparator());
+                bw.write("\n");
             }
 
-            file.close();
+            bw.close();
             System.out.println("Fichero generado correctamente.");
         } catch (IOException e) {
             e.printStackTrace();
